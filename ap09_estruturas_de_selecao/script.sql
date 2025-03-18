bb9935d9a4bf90913cbc22c5b1aee9edaa604ecc
Vitoria Cristina <vitcscoutinho@gmail.com>
2025-03-18 11:32:04 -0300

    learn(ap09): encerra o código 2.4.4

---
+++ ap09_estruturas_de_selecao/script.sql
@@ -0,0 +1,18 @@
+DO $$
+DECLARE
+    v_valor INT;
+BEGIN
+    v_valor := valor_aleatorio_entre(1, 30);
+    IF v_valor <= 20 THEN
+        RAISE NOTICE 'A metade de % é %', v_valor, v_valor / 2::FLOAT;
+    END IF;
+END;
+$$
+
+-- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+-- INT) RETURNS INT AS
+-- $$
+-- BEGIN
+-- RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+-- END;
+-- $$ LANGUAGE plpgsql;
